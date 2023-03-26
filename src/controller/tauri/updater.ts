31f0105f120b3270b0574f98a9455709d7aacc05
Omar Ezzat <omar.ezzat@email.com>
2023-03-26 15:12:51 +0200

    v0.0.13

--- src/controller/tauri/updater.ts
+++ src/controller/tauri/updater.ts
@@ -1,11 +1,20 @@
-import { checkUpdate, installUpdate } from '@tauri-apps/api/updater';
+import { checkUpdate, installUpdate, onUpdaterEvent } from '@tauri-apps/api/updater';
 import { relaunch } from '@tauri-apps/api/process';
 import { ask } from '@tauri-apps/api/dialog';
+import { getVersion, getName, getTauriVersion } from '@tauri-apps/api/app';
+
+const unlisten = await onUpdaterEvent((data) => {
+  console.log('Updater event', data);
+});
 
 export async function updateApp () {
   try {
-    const { shouldUpdate, manifest } = await checkUpdate()
-    console.log(shouldUpdate, manifest)
+    const { shouldUpdate, manifest } = await checkUpdate();
+    console.log(shouldUpdate, manifest);
+    console.log(`Current version: ${await getVersion()}`);
+    console.log(`App name: ${await getName()}`);
+    console.log(`Tauri version: ${await getTauriVersion()}`);
+    // await installUpdate().catch(console.error)
 
     if (shouldUpdate && await ask(`Install new version ${manifest?.version}?`, 'Updatea available')) {
       // display dialog
