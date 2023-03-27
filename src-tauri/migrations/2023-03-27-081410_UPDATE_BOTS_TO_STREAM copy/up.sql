ceb8433255d92484bb7c45b6bfaf4824361dee8b
Omar Ezzat <omar.ezzat@email.com>
2023-03-27 11:06:58 +0200

    update migration file

---
+++ src-tauri/migrations/2023-03-27-081410_UPDATE_BOTS_TO_STREAM copy/up.sql
@@ -0,0 +1,8 @@
+-- Your SQL goes here
+-- If a table in bots with the name 'OpenAI ChatGPT' exists then go to settings.api.body.stream and set to true
+-- only if exits
+
+UPDATE bots
+SET settings = json_set(settings, '$.api.body.stream', 'true') 
+WHERE name = 'OpenAI ChatGPT'
+AND settings IS NOT NULL;
